e50a8c63c163f81b4bcc2e5fc00f316fc3b9600c
vsc32366 <vsc32366@tier2-p-login-3.genius.hpc.kuleuven.be>
2025-10-27 14:56:47 +0100

    27 Oct 2025

--- src/main.py
+++ src/main.py
@@ -3,16 +3,29 @@ from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from myRAG import RAG
 from fastapi.responses import StreamingResponse
-rag = RAG()
+import os
+from dotenv import load_dotenv
+from pathlib import Path
+
+print("Start backend")
+env_path = Path(__file__).resolve().parent.parent / '.env' #find absolute path of ../
+load_dotenv(dotenv_path=env_path)
+
+rag = RAG(main_model_name = os.environ["MAIN_MODEL_NAME"],
+          num_ctx = int(os.environ["NUM_CTX"]),
+          vectordb_path = os.environ["VECTORDB_PATH"],
+          minor_models_device = os.environ["MINOR_MODELS_DEVICE"],
+          raw_documents_path = os.environ["RAW_DOCUMENTS_PATH"])
 rag.ready()
 print("RAG is ready.")
 
 app = FastAPI()
 
-origins = [
-    "http://127.0.0.1:5173",
-    "http://localhost:5173",
-]
+# origins = [
+#     "http://127.0.0.1:5173",
+#     "http://localhost:5173",
+# ]
+origins = [x.strip() for x in os.environ["ALLOW_ORIGINS"].split(",")]
 
 # Allow frontend to connect
 app.add_middleware(
